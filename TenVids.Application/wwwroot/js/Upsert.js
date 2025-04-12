1e7246d44a52ac7fc056385448a36643bcbff7fc
dinozoff1981 <tensaeg@yahoo.com>
2025-04-12 16:32:19 +0200

    image preview script added

--- TenVids.Application/wwwroot/js/Upsert.js
+++ TenVids.Application/wwwroot/js/Upsert.js
@@ -1,35 +1,19 @@
-﻿
-
-$(function () {
-
-    if ($('#videoId').val() == 0) {
-        $('#videoUpload').prop('disabled', true);
-        const url = $('#ImageUrl').val();
-        $('#previewImage').append(
-            $('<img>').attr({
-                'src': url,
-                'class': 'card-img-top rounded preview-image'
-
-            })
-        );
+﻿$(function () {
+    if ($('#videoId').val() > 0) {
+        // display preview image
+        $('#videoUpload').prop("disabled", true);
+        const url = $('#ImageUrl').val();  // Fixed: Changed 'imageUrl' to 'ImageUrl' to match your HTML
+        $('#previewImage').append(`<img src="${url}" class="card-img-top rounded preview-image" />`);
     }
-    });
+});
 
 $('#imageUpload').on('change', function () {
-    if (this.files && this.files[0]) {
+    if (this.files && this.files[0]) {  // Added check for files[0] to prevent errors
         $('#previewImage').empty();
         var reader = new FileReader();
-
         reader.onload = function (event) {
-            $('#previewImage').append(
-                $('<img>').attr({
-                    'src': event.target.result,
-                    'class': 'card-img-top rounded preview-image'
-                   
-                })
-            );
+            $('#previewImage').append(`<img src="${event.target.result}" class="card-img-top rounded preview-image" />`);
         };
-
         reader.readAsDataURL(this.files[0]);
     }
 });
