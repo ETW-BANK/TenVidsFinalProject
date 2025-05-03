a005da32f617d1448d659fddd71219cde924a5d0
dinozoff1981 <tensaeg@yahoo.com>
2025-05-03 23:38:07 +0200

    chek out revert changes

---
+++ TenVids.Application/wwwroot/js/Channels.js
@@ -0,0 +1,44 @@
+﻿// memberVideos.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize DataTable with configuration
+    const dataTable = $('#tbData').DataTable({
+        "ajax": {
+            "url": `/Member/GetMemberChannelVideos?channelId=${$('#tbData').data('channel-id')}`,
+            "dataSrc": 'result'
+        },
+        "columns": [
+            {
+                "data": {
+                    id: "id",
+                    title: "title",
+                    thumbnailUrl: "thumbnailUrl"
+                },
+                "render": function (data) {
+                    return `
+                        <div class="row">
+                            <div class="col-lg-6 text-center">
+                                <a href="/Video/WatchVideos/${data.id}" class="text-dark video-link">
+                                    ${data.title}
+                                </a>
+                            </div>
+                            <div class="col-lg-6 text-center">
+                                <a href="/Video/WatchVideos/${data.id}" class="thumbnail-link">
+                                    <img src="${data.thumbnailUrl}" class="card-img-top rounded preview-image" />
+                                </a>
+                            </div>
+                        </div>`;
+                }
+            },
+            {
+                "data": {
+                    createdAt: "createdAt",
+                    createdAtTimeAgo: "createdAtTimeAgo"
+                },
+                "render": function (data) {
+                    return `<span class="text-dark"><span class="hidden">${data.createdAt}</span>${data.createdAtTimeAgo}</span>`;
+                }
+            },
+            { "data": "numberOfViews" }
+        ]
+    });
+});
---
+++ TenVids.ViewModels/ModelErrorVm.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace TenVids.ViewModels
+{
+    public class ModelErrorVm
+    {
+        public string Key { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
